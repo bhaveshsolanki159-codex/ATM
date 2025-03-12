7d8724469cb017a29402cf04fcd3844bf833a267
bhaveshsolanki159-codex <bhaveshsolankibj159@gmail.com>
2025-03-12 17:29:44 +0530

    half done

---
+++ main.py
@@ -0,0 +1,58 @@
+class ATM:
+    def __init__(self):
+        self.balance = 0
+        self.pin = "" 
+        self.create_pin()
+
+        self.menu()
+
+    def create_pin(self):
+            self.pin = input("Enter your new PIN: ")
+            print("PIN created successfully!")
+
+    def menu(self):
+        print("Welcome to the ATM")
+        user_input = input('''1. Deposit \n2. Withdraw \n3. Check Balance \n4. Exit \nEnter your choice: ''')
+
+        # if user_input == "1":
+        #     self.create_pin()
+        if user_input == "1":
+            self.deposit()
+        elif user_input == "2":
+            self.withdraw()
+        elif user_input == "3": 
+            self.check_balance()
+        else:
+            print("Goodbye!")
+            exit()
+    
+    # def create_pin(self):
+    #     self.pin = input("Enter your new PIN: ")
+    #     print("PIN created successfully!")
+    
+    def deposit(self):
+        temp = int(input("Enter the PIN: "))
+        if temp == self.pin:
+            amount = int(input("Enter the amount you want to deposit: "))
+            self.balance += amount
+            print("Deposit successful!")
+        else:
+            print("Invalid PIN!")
+    
+    def withdraw(self):
+        temp = int(input("Enter the PIN: "))
+        if temp == self.pin:
+            amount = int(input("Enter the amount you want to withdraw: "))
+            if amount > self.balance:
+                print("Insufficient funds!")
+            else:
+                self.balance -= amount
+                print("Withdrawal successful!")
+        else:
+            print("Invalid PIN!")
+
+    def check_balance(self):
+        print(f"Your balance is: {self.balance}")
+
+atm = ATM()
+
